56526cfe507acf2e08b755afa6aac804a7e49148
esnya <2088693+esnya@users.noreply.github.com>
2023-08-16 14:33:56 +0900

    :sparkles: feat: Improve pyaudio_client.py

--- scripts/pyaudio_client.py
+++ scripts/pyaudio_client.py
@@ -1,54 +1,90 @@
 import asyncio
+import json
+import logging
 from collections import deque
+from typing import Optional
 
 import pyaudio
+from colorama import Fore
 from websockets.client import connect
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-async def pyaudio_client(url: str = "ws://localhost:8760"):
-    async with connect(url) as websocket:
 
-        async def writer():
-            pa = pyaudio.PyAudio()
+async def pyaudio_client(url: str = "ws://localhost:8760", retry: Optional[int] = None, buffer_size: int=4096, input_device_index: Optional[int]=None):
+    try:
+        logger.info(f"Connecting to {url}...")
+        async with connect(url) as websocket:
 
-            audio_queue = deque()
+            async def writer():
+                pa = pyaudio.PyAudio()
 
-            def stream_callback(in_data, frame_count, time_info, status):
-                audio_queue.append(in_data)
-                return (None, pyaudio.paContinue)
+                audio_queue = deque()
 
-            input_stream = pa.open(
-                rate=16000,
-                channels=1,
-                format=pyaudio.paFloat32,
-                input=True,
-                frames_per_buffer=4096,
-                start=True,
-                stream_callback=stream_callback,
-            )
+                def stream_callback(in_data, frame_count, time_info, status):
+                    audio_queue.append(in_data)
+                    return (None, pyaudio.paContinue)
 
-            while not websocket.closed or audio_queue:
-                if audio_queue:
-                    await websocket.send(audio_queue.popleft())
-                else:
-                    await asyncio.sleep(0)
+                input_stream = pa.open(
+                    rate=16000,
+                    channels=1,
+                    format=pyaudio.paFloat32,
+                    input=True,
+                    frames_per_buffer=buffer_size,
+                    start=True,
+                    input_device_index=input_device_index,
+                    stream_callback=stream_callback,
+                )
 
-            input_stream.stop_stream()
-            input_stream.close()
+                while not websocket.closed or audio_queue:
+                    if audio_queue:
+                        await websocket.send(audio_queue.popleft())
+                    else:
+                        await asyncio.sleep(0)
 
-        async def reader():
-            while not websocket.closed:
-                transcription = await websocket.recv()
-                print(transcription)
+                input_stream.stop_stream()
+                input_stream.close()
 
-        await asyncio.gather(writer(), reader())
+            async def reader():
+                while not websocket.closed:
+                    transcription = await websocket.recv()
+                    try:
+                        transcription = json.loads(transcription)
+                        is_final = transcription.get("is_final")
+                        is_valid = transcription.get("is_valid")
+                        if is_final:
+                            color = Fore.GREEN
+                        elif is_valid:
+                            color = Fore.YELLOW
+                        else:
+                            color = Fore.RED
+                        print(
+                            color
+                            + json.dumps(transcription, ensure_ascii=False)
+                            + Fore.RESET
+                        )
+                    except json.JSONDecodeError:
+                        print(transcription)
+
+            await asyncio.gather(writer(), reader())
+    except Exception as e:
+        logger.exception(e)
+
+        if retry is not None:
+            logger.info(f"Retrying in {retry} seconds...")
+            await asyncio.sleep(retry)
+            await pyaudio_client(url, retry)
 
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument("--url", type=str, default="ws://localhost:8760")
+    parser.add_argument("--url", "-u", type=str, default="ws://localhost:8760")
+    parser.add_argument("--retry", "-r", type=int)
+    parser.add_argument("--input-device-index", "-i", "-d" type=int)
+    parser.add_argument("--bufer-size", "-b", type=int, default=4096)
     args = parser.parse_args()
 
     asyncio.run(pyaudio_client(**args.__dict__))
